513d83bd3f05c1fffe5b721fbe4ec03e1b795dad
Uwe Fuchs <info@uwefuchs.com>
2014-03-15 10:16:03 +0100

    Java-Doc-comments added

--- open_ur-modules/open_ur-module-domain/src/main/java/org/openur/module/domain/userstructure/orgunit/IOrgUnitMember.java
+++ open_ur-modules/open_ur-module-domain/src/main/java/org/openur/module/domain/userstructure/orgunit/IOrgUnitMember.java
@@ -9,11 +9,33 @@ import org.openur.module.domain.security.IRole;
 public interface IOrgUnitMember
 	extends Comparable<IOrgUnitMember>
 {
+	/**
+	 * returns the personal identifier of the member.
+	 */
 	String getPersonId();
 
+	/**
+	 * returns the roles the member takes part in.
+	 * 
+	 * @return roles in a set (or empty set).
+	 */
 	Set<IRole> getRoles();
 
+	/**
+	 * indicates if a member has a certain role.
+	 * 
+	 * @param role : the role in which the member should take part in.
+	 * 
+	 * @return the member has the role.
+	 */
 	boolean hasRole(IRole role);
 	
+	/**
+	 * indicates if a member has a certain permission (within his/her set of roles).
+	 * 
+	 * @param role : the permission which the member should have.
+	 * 
+	 * @return the member has the permission.
+	 */
 	boolean hasPermission(IApplication app, IPermission permission);
 }
