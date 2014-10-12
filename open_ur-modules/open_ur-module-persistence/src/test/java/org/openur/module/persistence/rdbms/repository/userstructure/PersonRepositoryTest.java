e58fa8922eb955dff88110cb71c72b0337260a37
Uwe-Fuchs <info@uwefuchs.com>
2014-10-12 13:01:16 +0200

    new Test for PersonRepository (not yet implemented)

---
+++ open_ur-modules/open_ur-module-persistence/src/test/java/org/openur/module/persistence/rdbms/repository/userstructure/PersonRepositoryTest.java
@@ -0,0 +1,31 @@
+package org.openur.module.persistence.rdbms.repository.userstructure;
+
+import static org.junit.Assert.*;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openur.module.persistence.rdbms.config.RepositoryConfig;
+import org.openur.module.persistence.rdbms.entity.userstructure.PPerson;
+import org.openur.module.persistence.rdbms.repository.userstructure.PersonRepository;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration(classes = { RepositoryConfig.class })
+@Transactional
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class PersonRepositoryTest
+{
+	@Inject
+	private PersonRepository personRepository;
+
+	@Test
+	public void testRegisterPerson()
+	{
+		//PPerson person = new PPerson();
+	}
+}
