a14c85a9f4b0a07a0a06934284e678bed30ace7e
Uwe-Fuchs <info@uwefuchs.com>
2014-10-29 16:17:31 +0100

    remove compareTo-method

--- open_ur-modules/open_ur-module-domain-impl/src/main/java/org/openur/module/domain/userstructure/technicaluser/TechnicalUser.java
+++ open_ur-modules/open_ur-module-domain-impl/src/main/java/org/openur/module/domain/userstructure/technicaluser/TechnicalUser.java
@@ -1,6 +1,5 @@
 package org.openur.module.domain.userstructure.technicaluser;
 
-import org.apache.commons.lang3.builder.CompareToBuilder;
 import org.openur.module.domain.userstructure.UserStructureBase;
 
 public class TechnicalUser
@@ -12,16 +11,5 @@ public class TechnicalUser
 	TechnicalUser(TechnicalUserBuilder b)
 	{
 		super(b);
-	}
-
-	@Override
-	public int compareTo(ITechnicalUser other)
-	{
-		int comparison = new CompareToBuilder()
-												.append(this.getNumber(), other.getNumber())
-												.append(this.getStatus(), other.getStatus())
-												.toComparison();
-		
-		return comparison;
 	}
 }
