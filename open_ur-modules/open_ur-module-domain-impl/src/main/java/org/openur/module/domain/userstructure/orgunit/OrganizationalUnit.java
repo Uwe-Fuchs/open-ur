31090ced4cdfb723bb6f63f6c05a0e0b7f64b075
Uwe-Fuchs <info@uwefuchs.com>
2014-11-07 21:41:44 +0100

    change modifier of OrgUnit-Constructor from protected to package.

--- open_ur-modules/open_ur-module-domain-impl/src/main/java/org/openur/module/domain/userstructure/orgunit/OrganizationalUnit.java
+++ open_ur-modules/open_ur-module-domain-impl/src/main/java/org/openur/module/domain/userstructure/orgunit/OrganizationalUnit.java
@@ -18,7 +18,7 @@ public class OrganizationalUnit
 	private final EMailAddress emailAddress;
 
 	// constructor:
-	protected OrganizationalUnit(OrganizationalUnitBuilder b)
+	OrganizationalUnit(OrganizationalUnitBuilder b)
 	{
 		super(b);
 		this.name = b.getName();
