412241fb626f5600535fe731c67b98231b147688
Uwe-Fuchs <info@uwefuchs.com>
2014-11-21 21:08:35 +0100

    new Mapper-class for OrganizationalUnits, Test added.

--- open_ur-modules/open_ur-module-persistence-util/src/main/java/org/openur/module/persistence/rdbms/entity/userstructure/OrganizationalUnitMapper.java
+++ open_ur-modules/open_ur-module-persistence-util/src/main/java/org/openur/module/persistence/rdbms/entity/userstructure/OrganizationalUnitMapper.java
@@ -1,20 +1,141 @@
 package org.openur.module.persistence.rdbms.entity.userstructure;
 
+import java.util.Set;
+import java.util.UUID;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.openur.module.domain.userstructure.EMailAddress;
+import org.openur.module.domain.userstructure.orgunit.OrgUnitMember;
 import org.openur.module.domain.userstructure.orgunit.OrganizationalUnit;
+import org.openur.module.domain.userstructure.orgunit.OrganizationalUnitBuilder;
+import org.openur.module.domain.userstructure.person.Person;
 
 public class OrganizationalUnitMapper
 {
-	public static POrganizationalUnit mapFromImmutable(OrganizationalUnit immutable)
+	public static POrganizationalUnit mapFromImmutable(
+		OrganizationalUnit immutable, POrganizationalUnit rootOu,
+		POrganizationalUnit superOu)
 	{
 		POrganizationalUnit persistable = new POrganizationalUnit();
 
 		persistable.setNumber(immutable.getNumber());
 		persistable.setStatus(immutable.getStatus());
+		persistable.setRootOu(rootOu);
+		persistable.setSuperOu(superOu);
 		persistable.setName(immutable.getName());
 		persistable.setShortName(immutable.getShortName());
 		persistable.setDescription(immutable.getDescription());
-		persistable.setEmailAddress(immutable.getEmailAddress() != null ? immutable.getEmailAddress().getAsPlainEMailAddress() : null);
-		
+		persistable.setAddress(immutable.getAddress() != null ? AddressMapper
+			.mapFromImmutable(immutable.getAddress()) : null);
+		persistable.setEmailAddress(immutable.getEmailAddress() != null ? immutable
+			.getEmailAddress().getAsPlainEMailAddress() : null);
+
+		for (OrgUnitMember member : immutable.getMembers())
+		{
+			persistable.getMembers().add(new POrgUnitMember(persistable, PersonMapper.mapFromImmutable(member.getPerson())));
+		}
+
 		return persistable;
+	}
+
+	public static OrganizationalUnit mapFromEntity(
+		POrganizationalUnit persistable, OrganizationalUnit rootOu,
+		OrganizationalUnit superOu)
+	{
+		final String IDENTIFIER = StringUtils.isNotEmpty(persistable.getIdentifier()) ? persistable.getIdentifier() : UUID.randomUUID().toString();
+
+		OrganizationalUnitBuilder immutableBuilder = new OrganizationalUnitBuilder(IDENTIFIER);
+
+		if (rootOu != null)
+		{
+			immutableBuilder.rootOrgUnit(rootOu);
+			
+			if (superOu == null)
+			{
+				immutableBuilder.superOrgUnit(rootOu);
+			}
+		}
+
+		if (superOu != null)
+		{
+			immutableBuilder.superOrgUnit(superOu);
+		}
+
+		immutableBuilder
+			.number(persistable.getNumber())
+			.status(persistable.getStatus())
+			.name(persistable.getName())
+			.shortName(persistable.getShortName())
+			.description(persistable.getDescription())
+			.address(persistable.getAddress() != null ? AddressMapper.mapFromEntity(persistable.getAddress()) : null)
+			.emailAddress(StringUtils.isNotEmpty(persistable.getEmailAddress()) ? new EMailAddress(
+					persistable.getEmailAddress()) : null)
+			.creationDate(persistable.getCreationDate())
+			.lastModifiedDate(persistable.getLastModifiedDate());
+
+		for (POrgUnitMember pMember : persistable.getMembers())
+		{
+			Person pPerson = PersonMapper.mapFromEntity(pMember.getPerson());
+			immutableBuilder.addMember(new OrgUnitMember(pPerson, IDENTIFIER));
+		}
+
+		return immutableBuilder.build();
+	}
+
+	public static boolean immutableEqualsToEntity(OrganizationalUnit immutable,
+		POrganizationalUnit persistable)
+	{
+		if (!UserStructureBaseMapper.immutableEqualsToEntity(immutable, persistable))
+		{
+			return false;
+		}
+
+		if ((immutable.getAddress() != null || persistable.getAddress() != null)
+			&& !AddressMapper.immutableEqualsToEntity(immutable.getAddress(),	persistable.getAddress()))
+		{
+			return false;
+		}
+
+		boolean isEqual = new EqualsBuilder()
+			.append(immutable.getName(), persistable.getName())
+			.append(immutable.getShortName(), persistable.getShortName())
+			.append(immutable.getDescription(), persistable.getDescription())
+			.append(immutable.getEmailAddress() != null ? immutable.getEmailAddress()
+					.getAsPlainEMailAddress() : null, persistable.getEmailAddress())
+			.isEquals();
+
+		if (!isEqual)
+		{
+			return false;
+		}
+
+		for (POrgUnitMember pMember : persistable.getMembers())
+		{
+			OrgUnitMember member = findMemberInImmutable(pMember,	immutable.getMembers());
+
+			if (member == null
+				|| member.getPerson() == null
+				|| pMember.getPerson() == null
+				|| !PersonMapper.immutableEqualsToEntity(member.getPerson(), pMember.getPerson()))
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private static OrgUnitMember findMemberInImmutable(POrgUnitMember pMember, Set<OrgUnitMember> members)
+	{
+		for (OrgUnitMember member : members)
+		{
+			if (PersonMapper.immutableEqualsToEntity(member.getPerson(), pMember.getPerson()))
+			{
+				return member;
+			}
+		}
+
+		return null;
 	}
 }
--- open_ur-modules/open_ur-module-persistence-util/src/test/java/org/openur/module/persistence/rdbms/entity/userstructure/OrgUnitMapperTest.java
+++ open_ur-modules/open_ur-module-persistence-util/src/test/java/org/openur/module/persistence/rdbms/entity/userstructure/OrgUnitMapperTest.java
@@ -3,9 +3,15 @@ package org.openur.module.persistence.rdbms.entity.userstructure;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.UUID;
 
 import org.junit.Test;
+import org.openur.module.domain.userstructure.Address;
+import org.openur.module.domain.userstructure.Country;
+import org.openur.module.domain.userstructure.EMailAddress;
+import org.openur.module.domain.userstructure.Status;
+import org.openur.module.domain.userstructure.Address.AddressBuilder;
 import org.openur.module.domain.userstructure.orgunit.OrgUnitMember;
 import org.openur.module.domain.userstructure.orgunit.OrganizationalUnit;
 import org.openur.module.domain.userstructure.orgunit.OrganizationalUnitBuilder;
@@ -17,49 +23,113 @@ import org.openur.module.domain.userstructure.person.Title;
 
 public class OrgUnitMapperTest
 {
-//	@Test
-//	public void testMapFromImmutable()
-//	{
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testMapFromEntity()
-//	{
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testImmutableEqualsToEntity()
-//	{
-//		fail("Not yet implemented");
-//	}
-
 	@Test
-	public void testImmutableMemberEqualsToEntityMember()
+	public void testMapFromImmutable()
 	{
-		final String OU_ID = UUID.randomUUID().toString();
-		
 		Person pers1 = new PersonBuilder(Name.create(Gender.MALE, "Barack", "Obama"))
+			.number("persNo1")
 			.build();
 		
 		Person pers2 = new PersonBuilder(Name.create(Gender.FEMALE, Title.DR, "Angela", "Merkel"))
+			.number("persNo2")
 			.build();
-
-		OrgUnitMember m1 = new OrgUnitMember(pers1, OU_ID);
+		
+		final String OU_ID = UUID.randomUUID().toString();
+	
 		OrgUnitMember m2 = new OrgUnitMember(pers2, OU_ID);
+		OrgUnitMember m1 = new OrgUnitMember(pers1, OU_ID);		
 		
-		OrganizationalUnit orgUnit = new OrganizationalUnitBuilder(OU_ID)
+		OrganizationalUnit rootOu = new OrganizationalUnitBuilder()
+			.number("rootOuNo")
 			.build();
 		
-		POrganizationalUnit pOrgUnit = OrganizationalUnitMapper.mapFromImmutable(orgUnit, null, null);
-		PPerson pPers1 = PersonMapper.mapFromImmutable(pers1);
-		PPerson pPers2 = PersonMapper.mapFromImmutable(pers2);
+		OrganizationalUnit superOu = new OrganizationalUnitBuilder(rootOu)
+			.number("superOuNo")
+		  .superOrgUnit(rootOu)
+			.build();
 		
-		POrgUnitMember pMember1 = new POrgUnitMember(pOrgUnit, pPers1);
-		POrgUnitMember pMember2 = new POrgUnitMember(pOrgUnit, pPers2);
+		Address address = new AddressBuilder()
+			.country(Country.byCode("DE"))
+			.city("city_1")
+			.postcode("11")
+			.street("street_1")
+			.poBox("poBox_1")
+			.build();
 		
-		assertTrue(OrganizationalUnitMapper.immutableMemberEqualsToEntityMember(m1, pMember1));
-		assertTrue(OrganizationalUnitMapper.immutableMemberEqualsToEntityMember(m2, pMember2));
+		OrganizationalUnit orgUnit = new OrganizationalUnitBuilder(OU_ID, rootOu)
+			.number("orgUnitNo")
+			.status(Status.ACTIVE)
+			.name("staff department")
+			.shortName("stf")
+			.description("managing staff")
+			.superOrgUnit(superOu)
+			.orgUnitMembers(Arrays.asList(m1, m2))
+			.address(address)
+			.emailAddress(new EMailAddress("staff@company.com"))
+			.build();
+		
+		POrganizationalUnit pRootOu = OrganizationalUnitMapper.mapFromImmutable(rootOu, null, null);
+		assertTrue(OrganizationalUnitMapper.immutableEqualsToEntity(rootOu, pRootOu));
+		
+		POrganizationalUnit pSuperOu = OrganizationalUnitMapper.mapFromImmutable(superOu, pRootOu, null);
+		assertTrue(OrganizationalUnitMapper.immutableEqualsToEntity(superOu, pSuperOu));
+		
+		POrganizationalUnit pOrgUnit = OrganizationalUnitMapper.mapFromImmutable(orgUnit, pRootOu, pSuperOu);		
+		assertTrue(OrganizationalUnitMapper.immutableEqualsToEntity(orgUnit, pOrgUnit));
+	}
+
+	@Test
+	public void testMapFromEntity()
+	{
+		POrganizationalUnit pRootOu = new POrganizationalUnit();
+		pRootOu.setNumber("rootOuNo");
+		
+		POrganizationalUnit pSuperOu = new POrganizationalUnit();
+		pSuperOu.setNumber("superOuNo");
+		pSuperOu.setSuperOu(pRootOu);
+		pSuperOu.setRootOu(pRootOu);	
+		
+		POrganizationalUnit pOrgUnit = new POrganizationalUnit();
+		pOrgUnit.setNumber("orgUnitno");
+		pOrgUnit.setSuperOu(pSuperOu);
+		pOrgUnit.setRootOu(pRootOu);
+		pOrgUnit.setName("staff department");
+		pOrgUnit.setShortName("stf");
+		pOrgUnit.setDescription("managing staff");
+		pOrgUnit.setEmailAddress("staff@company.com");
+
+		PAddress pAddress = new PAddress();
+		pAddress.setCountryCode("DE");
+		pAddress.setCity("city_1");
+		pAddress.setPostcode("11");
+		pAddress.setStreet("street_1");
+		pAddress.setPoBox("poBox_1");
+		pOrgUnit.setAddress(pAddress);
+		
+		PPerson pPerson1 = new PPerson();
+		pPerson1.setNumber("persNo1");
+		pPerson1.setGender(Gender.MALE);
+		pPerson1.setFirstName("Barack");
+		pPerson1.setLastName("Obama");
+		
+		PPerson pPerson2 = new PPerson();
+		pPerson2.setNumber("persNo2");
+		pPerson2.setGender(Gender.FEMALE);
+		pPerson2.setTitle(Title.DR);
+		pPerson2.setFirstName("Angela");
+		pPerson2.setLastName("Merkel1");
+		
+		POrgUnitMember pMember1 = new POrgUnitMember(pOrgUnit, pPerson1);
+		POrgUnitMember pMember2 = new POrgUnitMember(pOrgUnit, pPerson2);
+		pOrgUnit.setMembers(new HashSet<POrgUnitMember>(Arrays.asList(pMember1, pMember2)));
+		
+		OrganizationalUnit rootOu = OrganizationalUnitMapper.mapFromEntity(pRootOu, null, null);
+		assertTrue(OrganizationalUnitMapper.immutableEqualsToEntity(rootOu, pRootOu));
+		
+		OrganizationalUnit superOu = OrganizationalUnitMapper.mapFromEntity(pSuperOu, rootOu, null);
+		assertTrue(OrganizationalUnitMapper.immutableEqualsToEntity(superOu, pSuperOu));
+		
+		OrganizationalUnit orgUnit = OrganizationalUnitMapper.mapFromEntity(pOrgUnit, rootOu, superOu);		
+		assertTrue(OrganizationalUnitMapper.immutableEqualsToEntity(orgUnit, pOrgUnit));
 	}
 }
