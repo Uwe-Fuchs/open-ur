22184d08d99485fe3610cc33337477806bb8b4b8
Uwe-Fuchs <info@uwefuchs.com>
2014-11-29 17:37:25 +0100

    add PermissionMapper plus Test

--- open_ur-modules/open_ur-module-persistence-util/src/main/java/org/openur/module/persistence/rdbms/entity/security/PermissionMapper.java
+++ open_ur-modules/open_ur-module-persistence-util/src/main/java/org/openur/module/persistence/rdbms/entity/security/PermissionMapper.java
@@ -1,6 +1,40 @@
 package org.openur.module.persistence.rdbms.entity.security;
 
+import org.apache.commons.lang3.StringUtils;
+import org.openur.module.domain.application.OpenURApplication;
+import org.openur.module.domain.security.authorization.OpenURPermission;
+import org.openur.module.domain.security.authorization.OpenURPermissionBuilder;
+import org.openur.module.persistence.rdbms.entity.application.ApplicationMapper;
+
 public class PermissionMapper
 {
-
+	public static PPermission mapFromImmutable(OpenURPermission immutable)
+	{
+		PPermission persistable = new PPermission();
+		
+		persistable.setPermissionName(immutable.getPermissionName());
+		persistable.setPermissionScope(immutable.getPermissionScope());
+		persistable.setDescription(immutable.getDescription());
+		persistable.setApplication(ApplicationMapper.mapFromImmutable(immutable.getApplication()));
+		
+		return persistable;
+	}
+	
+	public static OpenURPermission mapFromEntity(PPermission persistable)
+	{
+		OpenURApplication app = ApplicationMapper.mapFromEntity(persistable.getApplication());
+		OpenURPermissionBuilder immutableBuilder = new OpenURPermissionBuilder(persistable.getPermissionName(), app);
+		
+		if (StringUtils.isNotEmpty(persistable.getIdentifier()))
+		{
+			immutableBuilder.identifier(persistable.getIdentifier());
+		}
+		
+		return immutableBuilder
+			.permissionScope(persistable.getPermissionScope())
+			.description(persistable.getDescription())
+			.creationDate(persistable.getCreationDate())
+			.lastModifiedDate(persistable.getLastModifiedDate())
+			.build();
+	}
 }
--- open_ur-modules/open_ur-module-persistence-util/src/test/java/org/openur/module/persistence/rdbms/entity/security/PermissionMapperTest.java
+++ open_ur-modules/open_ur-module-persistence-util/src/test/java/org/openur/module/persistence/rdbms/entity/security/PermissionMapperTest.java
@@ -2,20 +2,63 @@ package org.openur.module.persistence.rdbms.entity.security;
 
 import static org.junit.Assert.*;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.junit.Test;
+import org.openur.module.domain.application.OpenURApplication;
+import org.openur.module.domain.application.OpenURApplicationBuilder;
+import org.openur.module.domain.security.authorization.OpenURPermission;
+import org.openur.module.domain.security.authorization.OpenURPermissionBuilder;
+import org.openur.module.domain.security.authorization.PermissionScope;
+import org.openur.module.persistence.rdbms.entity.AbstractEntityMapperTest;
+import org.openur.module.persistence.rdbms.entity.application.ApplicationMapper;
+import org.openur.module.persistence.rdbms.entity.application.PApplication;
 
 public class PermissionMapperTest
 {
-
 	@Test
 	public void testMapFromImmutable()
 	{
-		fail("Not yet implemented");
+		OpenURApplication app = new OpenURApplicationBuilder("appName").build();		
+		
+		OpenURPermission immutable = new OpenURPermissionBuilder("permName", app)
+				.permissionScope(PermissionScope.SELECTED)
+				.description("permission description")
+				.build();
+		
+		PPermission persistable = PermissionMapper.mapFromImmutable(immutable);
+		
+		assertTrue(immutableEqualsToEntity(immutable, persistable));
 	}
 
 	@Test
 	public void testMapFromEntity()
 	{
-		fail("Not yet implemented");
+		OpenURApplication app = new OpenURApplicationBuilder("appName").build();
+		PApplication pApp = ApplicationMapper.mapFromImmutable(app);
+		
+		PPermission persistable = new PPermission();
+		persistable.setApplication(pApp);
+		persistable.setPermissionName("permName");
+		persistable.setDescription("permDescription");
+		persistable.setPermissionScope(PermissionScope.SELECTED_SUB);
+		
+		OpenURPermission immutable = PermissionMapper.mapFromEntity(persistable);
+		
+		assertTrue(immutableEqualsToEntity(immutable, persistable));
+	}
+	
+	public static boolean immutableEqualsToEntity(OpenURPermission immutable, PPermission persistable)
+	{
+		if (!AbstractEntityMapperTest.immutableEqualsToEntityIdentifiable(immutable, persistable))
+		{
+			return false;
+		}
+	
+		return new EqualsBuilder()
+				.append(immutable.getApplication().getApplicationName(), persistable.getApplication().getApplicationName())
+				.append(immutable.getPermissionName(), persistable.getPermissionName())
+				.append(immutable.getPermissionScope(), persistable.getPermissionScope())
+				.append(immutable.getDescription(), persistable.getDescription())
+				.isEquals();
 	}
 }
