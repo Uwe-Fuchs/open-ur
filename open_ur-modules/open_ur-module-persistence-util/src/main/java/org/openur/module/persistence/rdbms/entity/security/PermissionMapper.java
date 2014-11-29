082bc3855a2d131833ef3ffb00e8f0e6f472ed5e
Uwe-Fuchs <info@uwefuchs.com>
2014-11-29 17:37:03 +0100

    add PermissionMapper plus Test

---
+++ open_ur-modules/open_ur-module-persistence-util/src/main/java/org/openur/module/persistence/rdbms/entity/security/PermissionMapper.java
@@ -0,0 +1,6 @@
+package org.openur.module.persistence.rdbms.entity.security;
+
+public class PermissionMapper
+{
+
+}
---
+++ open_ur-modules/open_ur-module-persistence-util/src/test/java/org/openur/module/persistence/rdbms/entity/security/PermissionMapperTest.java
@@ -0,0 +1,21 @@
+package org.openur.module.persistence.rdbms.entity.security;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PermissionMapperTest
+{
+
+	@Test
+	public void testMapFromImmutable()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMapFromEntity()
+	{
+		fail("Not yet implemented");
+	}
+}
