48e5dbf3a9b7d7848c1ca9ad9c7782175920d174
Uwe-Fuchs <info@uwefuchs.com>
2015-07-09 09:00:07 +0200

    new unittest for UserResource with arbitrary domain objects.

--- open_ur-remoting/open_ur-remoting-services-rest/src/test/java/org/openur/remoting/resource/userstructure/MockUserServicesFactory_ArbObj.java
+++ open_ur-remoting/open_ur-remoting-services-rest/src/test/java/org/openur/remoting/resource/userstructure/MockUserServicesFactory_ArbObj.java
@@ -1,6 +1,10 @@
 package org.openur.remoting.resource.userstructure;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 import org.glassfish.hk2.api.Factory;
+import org.mockito.Mockito;
 import org.openur.module.service.userstructure.IUserServices;
 
 public class MockUserServicesFactory_ArbObj
@@ -9,8 +13,18 @@ public class MockUserServicesFactory_ArbObj
 	@Override
 	public IUserServices provide()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		final IUserServices userServicesMock = Mockito.mock(IUserServices.class);
+		
+		Mockito.when(userServicesMock.findPersonById(UserResourceTest_ArbObj.UUID_1)).thenReturn(UserResourceTest_ArbObj.PERSON_1);
+		Mockito.when(userServicesMock.findPersonByNumber(UserResourceTest_ArbObj.NO_123)).thenReturn(UserResourceTest_ArbObj.PERSON_1);
+		Mockito.when(userServicesMock.obtainAllPersons()).thenReturn(
+					new HashSet<>(Arrays.asList(UserResourceTest_ArbObj.PERSON_1, UserResourceTest_ArbObj.PERSON_2)));
+		Mockito.when(userServicesMock.findTechnicalUserById(UserResourceTest_ArbObj.UUID_2)).thenReturn(UserResourceTest_ArbObj.TECH_USER_1);
+		Mockito.when(userServicesMock.findTechnicalUserByNumber(UserResourceTest_ArbObj.NO_456)).thenReturn(UserResourceTest_ArbObj.TECH_USER_1);
+		Mockito.when(userServicesMock.obtainAllTechnicalUsers()).thenReturn(
+					new HashSet<>(Arrays.asList(UserResourceTest_ArbObj.TECH_USER_1, UserResourceTest_ArbObj.TECH_USER_2)));
+
+		return userServicesMock;
 	}
 
 	@Override
