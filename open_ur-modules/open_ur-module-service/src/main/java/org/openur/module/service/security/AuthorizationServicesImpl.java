b6f065d4436febc49a32b533e2eb931ddc44528c
Uwe-Fuchs <info@uwefuchs.com>
2015-09-07 16:46:34 +0200

    new Authorization-Service for system-wide permissions, Refactoring corresponding unittest.

--- open_ur-modules/open_ur-module-service/src/main/java/org/openur/module/service/security/AuthorizationServicesImpl.java
+++ open_ur-modules/open_ur-module-service/src/main/java/org/openur/module/service/security/AuthorizationServicesImpl.java
@@ -1,6 +1,8 @@
 package org.openur.module.service.security;
 
 
+import java.util.Set;
+
 import javax.inject.Inject;
 
 import org.apache.commons.lang3.Validate;
@@ -35,25 +37,44 @@ public class AuthorizationServicesImpl
 		
 		return hasPerm;
 	}
-
-	@Override
-	public Boolean hasPermission(String userId, String orgUnitId, String permissionText, String applicationName)
+	
+	private boolean lookupDomainObjectsAndCheckIfUserHasPermission(
+		String personId, String permissionText, String applicationName, IAuthorizableOrgUnit orgUnit)
 	{
-		Validate.notEmpty(userId, "user-id must not be empty!");
-		Validate.notEmpty(orgUnitId, "org-unit-id must not be empty!");
+		Validate.notEmpty(personId, "person-id must not be empty!");
 		Validate.notEmpty(permissionText, "permission-text must not be empty!");
 		Validate.notEmpty(applicationName, "application-name must not be empty!");
 		
-		IPerson person = userServices.findPersonById(userId);
-		Validate.notNull(person, String.format("No person found for userId '%s'!", userId));
-		
-		IAuthorizableOrgUnit orgUnit = orgUnitServices.findOrgUnitById(orgUnitId);
-		Validate.notNull(orgUnit, String.format("No org-unit found for orgUnitId '%s'!", orgUnitId));
+		IPerson person = userServices.findPersonById(personId);
+		Validate.notNull(person, String.format("No person found for ID '%s'!", personId));
 		
 		IPermission permission = securityDomainServices.findPermission(permissionText, applicationName);
 		Validate.notNull(
 			permission, String.format("No permission found with permission-text '%s' and application-name '%s'!", permissionText, applicationName));
 		
 		return internalHasPermission(person, orgUnit, permission);
+	}
+
+	@Override
+	public Boolean hasPermission(String personId, String orgUnitId, String permissionText, String applicationName)
+	{
+		Validate.notEmpty(orgUnitId, "org-unit-id must not be empty!");
+		
+		IAuthorizableOrgUnit orgUnit = orgUnitServices.findOrgUnitById(orgUnitId);
+		Validate.notNull(orgUnit, String.format("No org-unit found for orgUnitId '%s'!", orgUnitId));
+		
+		return lookupDomainObjectsAndCheckIfUserHasPermission(personId, permissionText, applicationName, orgUnit);
+	}
+
+	@Override
+	public Boolean hasPermission(String personId, String permissionText, String applicationName)
+	{		
+		Set<IAuthorizableOrgUnit> rootOus = orgUnitServices.obtainRootOrgUnits();
+		Validate.validState(!rootOus.isEmpty(), "There must be at least one root organizational-unit within the system!");
+		
+		// take first root-ou, for system-wide permissions are placed (redundantly) in ALL root-ou's:
+		IAuthorizableOrgUnit orgUnit = rootOus.stream().findFirst().get();
+		
+		return lookupDomainObjectsAndCheckIfUserHasPermission(personId, permissionText, applicationName, orgUnit);
 	}
 }
--- open_ur-modules/open_ur-module-service/src/main/java/org/openur/module/service/security/IAuthorizationServices.java
+++ open_ur-modules/open_ur-module-service/src/main/java/org/openur/module/service/security/IAuthorizationServices.java
@@ -5,14 +5,26 @@ package org.openur.module.service.security;
 public interface IAuthorizationServices
 {
   /**
-   * checks if a user has a certain (app-based) permission within an organizational-unit.
+   * checks if a person has a certain (app-based) permission within an organizational-unit.
+   * Hint: Use this if work with permission-based, org-unit-related authorization.
    * 
-   * @param userId
+   * @param personId
    * @param orgUnitId
    * @param permissionText
    * @param applicationName
    * 
-   * @return the user has the permission.
+   * @return the person has the permission within the given organizational-unit.
    */
-  Boolean hasPermission(String userId, String orgUnitId, String permissionText, String applicationName);
-}
+  Boolean hasPermission(String personId, String orgUnitId, String permissionText, String applicationName);
+  
+  /**
+   * checks if a person has a certain (app-based) permission.
+   * Hint: use this if you work with permission-based, but NOT org-unit-related authorization.
+   * 
+   * @param personId
+   * @param permissionText
+   * @param applicationName
+   * 
+   * @return the person has the permission.
+   */
+  Boolean hasPermission(String personId, String permissionText, String applicationName);}
