e121bc8b365a3ae6ddbcf87f74df98529872fe50
Uwe-Fuchs <info@uwefuchs.com>
2015-10-28 09:23:28 +0100

    removed old module open_ur-module-persistence-mapping-api-entity.

--- open_ur-modules/open_ur-module-persistence-mapping-api-entity/src/main/java/org/openur/module/persistence/rdbms/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package org.openur.module.persistence.rdbms.repository;
-
-import org.openur.module.persistence.rdbms.entity.PAddress;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository
-	extends JpaRepository<PAddress, Long>
-{
-}
