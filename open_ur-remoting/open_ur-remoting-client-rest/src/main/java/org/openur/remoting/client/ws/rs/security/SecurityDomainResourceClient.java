bd6a2e44cf7cb3bc0a8e33b98f46ce487f75b2fc
Uwe-Fuchs <info@uwefuchs.com>
2015-11-01 12:52:03 +0100

    new SecurityDomainResourceClient plus Test.

--- open_ur-remoting/open_ur-remoting-client-rest/src/main/java/org/openur/remoting/client/ws/rs/security/SecurityDomainResourceClient.java
+++ open_ur-remoting/open_ur-remoting-client-rest/src/main/java/org/openur/remoting/client/ws/rs/security/SecurityDomainResourceClient.java
@@ -1,69 +1,129 @@
 package org.openur.remoting.client.ws.rs.security;
 
+import static org.openur.remoting.resource.security.SecurityDomainResource.ALL_PERMISSIONS_RESOURCE_PATH;
+import static org.openur.remoting.resource.security.SecurityDomainResource.ALL_ROLES_RESOURCE_PATH;
+import static org.openur.remoting.resource.security.SecurityDomainResource.PERMISSIONS_PER_APP_RESOURCE_PATH;
+import static org.openur.remoting.resource.security.SecurityDomainResource.PERMISSION_PER_ID_RESOURCE_PATH;
+import static org.openur.remoting.resource.security.SecurityDomainResource.PERMISSION_PER_TEXT_RESOURCE_PATH;
+import static org.openur.remoting.resource.security.SecurityDomainResource.ROLE_PER_ID_RESOURCE_PATH;
+import static org.openur.remoting.resource.security.SecurityDomainResource.ROLE_PER_NAME_RESOURCE_PATH;
+import static org.openur.remoting.resource.security.SecurityDomainResource.SECURITY_DOMAIN_RESOURCE_PATH;
+
 import java.util.Set;
 
+import javax.inject.Inject;
+import javax.ws.rs.core.GenericType;
+
+import org.glassfish.hk2.utilities.reflection.ParameterizedTypeImpl;
 import org.openur.module.domain.security.authorization.IPermission;
 import org.openur.module.domain.security.authorization.IRole;
+import org.openur.module.domain.security.authorization.OpenURPermission;
+import org.openur.module.domain.security.authorization.OpenURRole;
 import org.openur.module.service.security.ISecurityDomainServices;
 import org.openur.remoting.client.ws.rs.AbstractResourceClient;
+import org.openur.remoting.xchange.rest.providers.json.IdentifiableEntitySetProvider;
+import org.openur.remoting.xchange.rest.providers.json.PermissionProvider;
+import org.openur.remoting.xchange.rest.providers.json.RoleProvider;
 
 public class SecurityDomainResourceClient
 	extends AbstractResourceClient
 	implements ISecurityDomainServices
 {
-
+	@Inject
 	public SecurityDomainResourceClient(String baseUrl)
 	{
-		super(baseUrl);
-		// TODO Auto-generated constructor stub
+		super(baseUrl, PermissionProvider.class, RoleProvider.class, IdentifiableEntitySetProvider.class);
 	}
 
 	@Override
 	public IRole findRoleById(String roleId)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		String url = new StringBuilder()
+				.append(getBaseUrl())
+				.append(SECURITY_DOMAIN_RESOURCE_PATH)
+				.append(ROLE_PER_ID_RESOURCE_PATH)
+				.append(roleId)
+				.toString();
+		
+		return performRestCall(url, OpenURRole.class);
 	}
 
 	@Override
 	public IRole findRoleByName(String roleName)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		String url = new StringBuilder()
+				.append(getBaseUrl())
+				.append(SECURITY_DOMAIN_RESOURCE_PATH)
+				.append(ROLE_PER_NAME_RESOURCE_PATH)
+				.append(roleName)
+				.toString();
+		
+		return performRestCall(url, OpenURRole.class);
 	}
 
 	@Override
 	public Set<IRole> obtainAllRoles()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		String url = new StringBuilder()
+				.append(getBaseUrl())
+				.append(SECURITY_DOMAIN_RESOURCE_PATH)
+				.append(ALL_ROLES_RESOURCE_PATH)
+				.toString();
+		
+		return performRestCall(url, new GenericType<Set<IRole>>(new ParameterizedTypeImpl(Set.class, OpenURRole.class)));
 	}
 
 	@Override
 	public IPermission findPermissionById(String permissionId)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		String url = new StringBuilder()
+				.append(getBaseUrl())
+				.append(SECURITY_DOMAIN_RESOURCE_PATH)
+				.append(PERMISSION_PER_ID_RESOURCE_PATH)
+				.append(permissionId)
+				.toString();
+		
+		return performRestCall(url, OpenURPermission.class);
 	}
 
 	@Override
 	public IPermission findPermission(String permissionText, String applicationName)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		String url = new StringBuilder()
+				.append(getBaseUrl())
+				.append(SECURITY_DOMAIN_RESOURCE_PATH)
+				.append(PERMISSION_PER_TEXT_RESOURCE_PATH)
+				.append("?text=")
+				.append(permissionText)
+				.append("&appName=")
+				.append(applicationName)
+				.toString();
+		
+		return performRestCall(url, OpenURPermission.class);
 	}
 
 	@Override
 	public Set<IPermission> obtainPermissionsForApp(String applicationName)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		String url = new StringBuilder()
+				.append(getBaseUrl())
+				.append(SECURITY_DOMAIN_RESOURCE_PATH)
+				.append(PERMISSIONS_PER_APP_RESOURCE_PATH)
+				.append(applicationName)
+				.toString();
+		
+		return performRestCall(url, new GenericType<Set<IPermission>>(new ParameterizedTypeImpl(Set.class, OpenURPermission.class)));
 	}
 
 	@Override
 	public Set<IPermission> obtainAllPermissions()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		String url = new StringBuilder()
+				.append(getBaseUrl())
+				.append(SECURITY_DOMAIN_RESOURCE_PATH)
+				.append(ALL_PERMISSIONS_RESOURCE_PATH)
+				.toString();
+		
+		return performRestCall(url, new GenericType<Set<IPermission>>(new ParameterizedTypeImpl(Set.class, OpenURPermission.class)));
 	}
 }
--- open_ur-remoting/open_ur-remoting-client-rest/src/test/java/org/openur/remoting/client/ws/rs/security/SecurityDomainResourceClientTest.java
+++ open_ur-remoting/open_ur-remoting-client-rest/src/test/java/org/openur/remoting/client/ws/rs/security/SecurityDomainResourceClientTest.java
@@ -2,66 +2,162 @@ package org.openur.remoting.client.ws.rs.security;
 
 import static org.junit.Assert.*;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import javax.ws.rs.core.Application;
 
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
+import org.openur.domain.testfixture.testobjects.TestObjectContainer;
+import org.openur.module.domain.security.authorization.IPermission;
+import org.openur.module.domain.security.authorization.IRole;
+import org.openur.module.domain.security.authorization.OpenURPermission;
+import org.openur.module.domain.security.authorization.OpenURRole;
+import org.openur.module.domain.utils.common.DomainObjectHelper;
+import org.openur.module.domain.utils.compare.PermissionComparer;
+import org.openur.module.domain.utils.compare.RoleComparer;
+import org.openur.module.service.security.ISecurityDomainServices;
+import org.openur.remoting.resource.security.MockSecurityDomainServicesFactory;
+import org.openur.remoting.resource.security.SecurityDomainResource;
 
 public class SecurityDomainResourceClientTest
 	extends JerseyTest
 {
+	private ISecurityDomainServices securityDomainServices;
+	
 	@Override
 	protected Application configure()
 	{
-		// TODO Auto-generated method stub
-		return super.configure();
-	}
+		// Http-Testserver:
+		AbstractBinder binder = new AbstractBinder()
+		{
+			@Override
+			protected void configure()
+			{
+				bindFactory(MockSecurityDomainServicesFactory.class).to(ISecurityDomainServices.class);
+			}
+		};
 
-	@Test
-	public void testSecurityDomainResourceClient()
-	{
-		fail("Not yet implemented");
+		ResourceConfig config = new ResourceConfig(SecurityDomainResource.class)
+				.register(binder);
+		
+		// Client:
+		securityDomainServices = new SecurityDomainResourceClient("http://localhost:9998/");		
+		
+		for (Class<?> provider : ((SecurityDomainResourceClient) securityDomainServices).getProviders())
+		{
+			config.register(provider);
+		}
+
+		return config;
 	}
 
 	@Test
 	public void testFindRoleById()
 	{
-		fail("Not yet implemented");
+		IRole r = securityDomainServices.findRoleById(TestObjectContainer.ROLE_X.getIdentifier());
+
+		assertNotNull(r);
+		System.out.println("Result: " + r);
+		
+		assertTrue(new RoleComparer().objectsAreEqual((OpenURRole) r, TestObjectContainer.ROLE_X));
 	}
 
 	@Test
 	public void testFindRoleByName()
 	{
-		fail("Not yet implemented");
+		IRole r = securityDomainServices.findRoleByName(TestObjectContainer.ROLE_X.getRoleName());
+
+		assertNotNull(r);
+		System.out.println("Result: " + r);
+		
+		assertTrue(new RoleComparer().objectsAreEqual((OpenURRole) r, TestObjectContainer.ROLE_X));
 	}
 
 	@Test
 	public void testObtainAllRoles()
 	{
-		fail("Not yet implemented");
+		Set<IRole> roles = securityDomainServices.obtainAllRoles();
+
+		assertFalse(roles.isEmpty());
+		assertEquals(3, roles.size());
+		System.out.println("Result: " + roles);
+		
+		Set<OpenURRole> resultSet = roles.stream().map(r -> (OpenURRole) r).collect(Collectors.toSet());
+		
+		OpenURRole r = DomainObjectHelper.findIdentifiableEntityInCollection(resultSet, TestObjectContainer.ROLE_X.getIdentifier());
+		assertTrue(new RoleComparer().objectsAreEqual(TestObjectContainer.ROLE_X, r));
+		r = DomainObjectHelper.findIdentifiableEntityInCollection(resultSet, TestObjectContainer.ROLE_Y.getIdentifier());
+		assertTrue(new RoleComparer().objectsAreEqual(TestObjectContainer.ROLE_Y, r));
+		r = DomainObjectHelper.findIdentifiableEntityInCollection(resultSet, TestObjectContainer.ROLE_Z.getIdentifier());
+		assertTrue(new RoleComparer().objectsAreEqual(TestObjectContainer.ROLE_Z, r));
 	}
 
 	@Test
 	public void testFindPermissionById()
 	{
-		fail("Not yet implemented");
+		IPermission p = securityDomainServices.findPermissionById(TestObjectContainer.PERMISSION_1_A.getIdentifier());
+
+		assertNotNull(p);
+		System.out.println("Result: " + p);
+		
+		assertTrue(new PermissionComparer().objectsAreEqual((OpenURPermission) p, TestObjectContainer.PERMISSION_1_A));
 	}
 
 	@Test
 	public void testFindPermission()
 	{
-		fail("Not yet implemented");
+		IPermission p = securityDomainServices.findPermission(
+				TestObjectContainer.PERMISSION_1_A.getPermissionText(), TestObjectContainer.APP_A.getApplicationName());
+
+		assertNotNull(p);
+		System.out.println("Result: " + p);
+		
+		assertTrue(new PermissionComparer().objectsAreEqual((OpenURPermission) p, TestObjectContainer.PERMISSION_1_A));
 	}
 
 	@Test
 	public void testObtainPermissionsForApp()
 	{
-		fail("Not yet implemented");
+		Set<IPermission> permissions = securityDomainServices.obtainPermissionsForApp(TestObjectContainer.APP_A.getApplicationName());
+
+		assertFalse(permissions.isEmpty());
+		assertEquals(2, permissions.size());
+		System.out.println("Result: " + permissions);
+		
+		Set<OpenURPermission> resultSet = permissions.stream().map(p -> (OpenURPermission) p).collect(Collectors.toSet());
+		
+		OpenURPermission p = DomainObjectHelper.findIdentifiableEntityInCollection(resultSet, TestObjectContainer.PERMISSION_1_A.getIdentifier());
+		assertTrue(new PermissionComparer().objectsAreEqual(TestObjectContainer.PERMISSION_1_A, p));
+		p = DomainObjectHelper.findIdentifiableEntityInCollection(resultSet, TestObjectContainer.PERMISSION_2_A.getIdentifier());
+		assertTrue(new PermissionComparer().objectsAreEqual(TestObjectContainer.PERMISSION_2_A, p));
 	}
 
 	@Test
 	public void testObtainAllPermissions()
 	{
-		fail("Not yet implemented");
+		Set<IPermission> permissions = securityDomainServices.obtainAllPermissions();
+
+		assertFalse(permissions.isEmpty());
+		assertEquals(6, permissions.size());
+		System.out.println("Result: " + permissions);
+		
+		Set<OpenURPermission> resultSet = permissions.stream().map(p -> (OpenURPermission) p).collect(Collectors.toSet());
+		
+		OpenURPermission p = DomainObjectHelper.findIdentifiableEntityInCollection(resultSet, TestObjectContainer.PERMISSION_1_A.getIdentifier());
+		assertTrue(new PermissionComparer().objectsAreEqual(TestObjectContainer.PERMISSION_1_A, p));
+		p = DomainObjectHelper.findIdentifiableEntityInCollection(resultSet, TestObjectContainer.PERMISSION_1_B.getIdentifier());
+		assertTrue(new PermissionComparer().objectsAreEqual(TestObjectContainer.PERMISSION_1_B, p));
+		p = DomainObjectHelper.findIdentifiableEntityInCollection(resultSet, TestObjectContainer.PERMISSION_2_A.getIdentifier());
+		assertTrue(new PermissionComparer().objectsAreEqual(TestObjectContainer.PERMISSION_2_A, p));
+		p = DomainObjectHelper.findIdentifiableEntityInCollection(resultSet, TestObjectContainer.PERMISSION_2_B.getIdentifier());
+		assertTrue(new PermissionComparer().objectsAreEqual(TestObjectContainer.PERMISSION_2_B, p));
+		p = DomainObjectHelper.findIdentifiableEntityInCollection(resultSet, TestObjectContainer.PERMISSION_1_C.getIdentifier());
+		assertTrue(new PermissionComparer().objectsAreEqual(TestObjectContainer.PERMISSION_1_C, p));
+		p = DomainObjectHelper.findIdentifiableEntityInCollection(resultSet, TestObjectContainer.PERMISSION_2_C.getIdentifier());
+		assertTrue(new PermissionComparer().objectsAreEqual(TestObjectContainer.PERMISSION_2_C, p));
 	}
 }
