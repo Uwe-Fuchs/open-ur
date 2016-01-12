c05ac43361b0cb381da653981c8e52c72f3be5a7
Uwe-Fuchs <info@uwefuchs.com>
2016-01-12 11:42:21 +0100

    new entity PUserAccount.

--- open_ur-modules/open_ur-module-persistence-entity/src/main/java/org/openur/module/persistence/rdbms/entity/PUserAccount.java
+++ open_ur-modules/open_ur-module-persistence-entity/src/main/java/org/openur/module/persistence/rdbms/entity/PUserAccount.java
@@ -1,7 +1,17 @@
 package org.openur.module.persistence.rdbms.entity;
 
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -10,16 +20,27 @@ import org.apache.commons.lang3.Validate;
 @Entity(name="USER_ACCOUNT")
 @Table(uniqueConstraints={@UniqueConstraint(name="UNQ_USERNAME", columnNames={"USERNAME"})})
 public class PUserAccount
-	extends AbstractOpenUrPersistable
+	implements Serializable
 {
 	private static final long serialVersionUID = -2236145073084159919L;
 
 	// properties:
+	@Id
+	@OneToOne(fetch=FetchType.EAGER, cascade=CascadeType.PERSIST)
+	@JoinColumn(name="ID", referencedColumnName="ID")
+	private PUserStructureBase userStructureBase;
+	
 	@Column(name="USERNAME", length=50, nullable=false)
   private String userName;
 
 	@Column(name="PASSWORD", length=50, nullable=false)
   private String passWord;
+
+	@Column(name="CREATED", nullable=false)
+	private LocalDateTime creationDate;
+
+	@Column(name="MODIFIED")
+	private LocalDateTime lastModifiedDate;
 	
 	@Column(name="SALT")
 	private String salt;
@@ -43,6 +64,28 @@ public class PUserAccount
 	public String getPassWord()
 	{
 		return passWord;
+	}
+
+	public LocalDateTime getCreationDate()
+	{
+		return creationDate;
+	}
+
+	public LocalDateTime getLastModifiedDate()
+	{
+		return lastModifiedDate;
+	}
+
+	@PrePersist
+	void setCreationDate()
+	{
+		this.creationDate = LocalDateTime.now();
+	}
+
+	@PreUpdate
+	void setLastModifiedDate()
+	{
+		this.lastModifiedDate = LocalDateTime.now();
 	}
 
 	// constructors:
