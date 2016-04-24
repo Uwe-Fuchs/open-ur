1b4e1282c9f709a3b70aadafb9fc4080da6bc97a
Uwe-Fuchs <info@uwefuchs.com>
2016-04-24 17:38:54 +0200

    fix module-dependencies.

--- open_ur-modules/open_ur-module-integration-security-shiro/src/main/java/org/openur/module/integration/security/shiro/OpenUrRdbmsRealmMock.java
+++ open_ur-modules/open_ur-module-integration-security-shiro/src/main/java/org/openur/module/integration/security/shiro/OpenUrRdbmsRealmMock.java
@@ -1,5 +1,7 @@
 package org.openur.module.integration.security.shiro;
 
+import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
@@ -7,8 +9,7 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.util.ByteSource;
-import org.openur.module.domain.security.authentication.UserAccount;
-import org.openur.module.domain.security.authentication.UserAccountBuilder;
+import org.openur.module.domain.security.authentication.IUserAccount;
 
 /**
  * Mock-Object for {@link OpenUrRdbmsRealm}, for the {@link OpenUrRdbmsRealm#getAuthenticationInfo()}-Method is final and hence cannot be mocked via Mockito.
@@ -25,11 +26,11 @@ public class OpenUrRdbmsRealmMock
 	public static final String PERSON_UUID_1 = UUID.randomUUID().toString();
 	public static final String USER_NAME_1 = "userName_1";
 	public static final String PASSWORD_1 = "password_1";
-	public static final UserAccount PERSON_1_ACCOUNT;
+	public static final MyUserAccount PERSON_1_ACCOUNT;
 	public static final String TECH_USER_UUID_2 = UUID.randomUUID().toString();
 	public static final String USER_NAME_2 = "userName_2";
 	public static final String PASSWORD_2 = "password_2";	
-	public static final UserAccount TECH_USER_2_ACCOUNT;
+	public static final MyUserAccount TECH_USER_2_ACCOUNT;
 	public static final UsernamePwAuthenticationInfo AUTH_INFO;
 	public static final UsernamePasswordToken USERNAME_PW_TOKEN;
 	public static final UsernamePasswordToken TOKEN_WITH_WRONG_PW;
@@ -43,15 +44,15 @@ public class OpenUrRdbmsRealmMock
 		AUTH_INFO = new UsernamePwAuthenticationInfo(USER_NAME_1, PASSWORD_1.toCharArray(), REALM_NAME);
 		AUTH_INFO.setCredentialsSalt(ByteSource.Util.bytes(SALT_BASE));
 		
-		PERSON_1_ACCOUNT = new UserAccountBuilder(USER_NAME_1, PASSWORD_1)
-				.identifier(PERSON_UUID_1)
-				.salt(SALT_BASE)
-				.build();
+		PERSON_1_ACCOUNT = new MyUserAccount();
+		PERSON_1_ACCOUNT.setUserName(USER_NAME_1);
+		PERSON_1_ACCOUNT.setPassWord(PASSWORD_1);
+		PERSON_1_ACCOUNT.setSalt(SALT_BASE);
 		
-		TECH_USER_2_ACCOUNT = new UserAccountBuilder(USER_NAME_2, PASSWORD_2)
-				.identifier(TECH_USER_UUID_2)
-				.salt(SALT_BASE)
-				.build();
+		TECH_USER_2_ACCOUNT = new MyUserAccount();
+		TECH_USER_2_ACCOUNT.setUserName(USER_NAME_2);
+		TECH_USER_2_ACCOUNT.setPassWord(PASSWORD_2);
+		TECH_USER_2_ACCOUNT.setSalt(SALT_BASE);
 	}
 
 	@Override
@@ -76,5 +77,68 @@ public class OpenUrRdbmsRealmMock
 	public String getName()
 	{
 		return REALM_NAME;
+	}
+	
+	public static class MyUserAccount
+		implements IUserAccount, Serializable
+	{
+		private static final long serialVersionUID = 7165821312250746670L;
+		
+		// properties:
+		private String id = UUID.randomUUID().toString();
+		private String userName;
+		private String passWord;
+		private String salt;
+
+		@Override
+		public String getIdentifier()
+		{
+			return id;
+		}
+
+		@Override
+		public String getUserName()
+		{
+			return userName;
+		}
+
+		public void setUserName(String userName)
+		{
+			this.userName = userName;
+		}
+
+		@Override
+		public String getPassWord()
+		{
+			return passWord;
+		}
+
+		public void setPassWord(String passWord)
+		{
+			this.passWord = passWord;
+		}
+
+		@Override
+		public String getSalt()
+		{
+			return salt;
+		}
+
+		public void setSalt(String salt)
+		{
+			this.salt = salt;
+		}
+
+		@Override
+		public LocalDateTime getLastModifiedDate()
+		{
+			return null;
+		}
+
+		@Override
+		public LocalDateTime getCreationDate()
+		{
+			return null;
+		}
 	}
 }
